4b1cc0074710bf3428a8b2ef8a33e128b7d12093
daudujohn <daudujoe@gmail.com>
2023-03-29 11:36:43 +0100

    chore: Add eslint package for code style enforcement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-PORT = process.env['PORT'] || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
+  console.log(`Listening on port ${PORT}...`)
 })
