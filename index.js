648607999b6e388161f5dbcdf690bdb7ec4b1f45
daudujohn <daudujoe@gmail.com>
2023-03-31 14:48:20 +0100

    chore: update index.js

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express')
+require('./utils/db.config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const bodyParser = require('body-parser')
+const {User, validateUser} = require('./models/user');
+
 const app = express()
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', { message: null})
+})
+
+app.post('/register', async (req, res) => {
+    const {error} = validateUser(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+    
+    const user = new User(req.body)
+    await user.save()
+
+    res.render('register', { message: 'Registration Successful'})
+})
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
-})
+
+if (require.main === module){
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`)
+    })
+}
+
+module.exports = app
