94e61251161e2702442edb079c27aad2230e0ebf
daudujohn <daudujoe@gmail.com>
2023-04-03 11:37:53 +0100

    chore: update register route

--- routes/register.js
+++ routes/register.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const {User, validateUser} = require('../models/user');
+const UserController = require('../controllers/register');
+const {validateUser} = require('../models/user');
+const {joiFormatter, mongooseFormatter} = require('../utils/validationFormatter');
 
 router.get('/', (req, res) => {
-    res.render('register', { message: null })
+    res.render('register', {message: {}, errors: {}, formData: {}})
 })
 
 router.post('/', async (req, res) => {
-    const {error} = validateUser(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-    
-    const user = new User(req.body)
-    await user.save()
+    try{
+        const {error} = validateUser(req.body)
+        if (error) return res.render('register', {
+            message: {
+                type: 'error', 
+                body: 'Validation Error'
+            },  
+            errors: joiFormatter(error) ?? {}, 
+            formData: req.body
+        })
 
-    res.render('register', { message: 'Registration Successful' })
+        await UserController.addUser(req.body)
+        return res.render('register', {
+            message: {
+                type: 'success', 
+                body: 'Registration Successful'
+            },  
+            errors: {}, 
+            formData: req.body
+        })
+    }
+    catch(e){
+        res.status(500).render('register', {
+            message: {
+                type: 'error', 
+                body: 'Validation Error'
+            },  
+            errors: mongooseFormatter(e), 
+            formData: req.body
+        })
+    }
 })
 
 module.exports = router
