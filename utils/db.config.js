914a877a34b988118a798d7212de1610f4d31835
daudujohn <daudujoe@gmail.com>
2023-04-05 14:27:02 +0100

    refactor: update db.config.js

--- utils/db.config.js
+++ utils/db.config.js
@@ -1,9 +1,13 @@
 const config = require('config');
 const mongoose = require('mongoose');
 
-mongoose.connect(config.get('db.conn', {
+module.exports = mongoose.connect(config.get('db.conn'), {
     useNewUrlParser: true, 
-    useUnifiedTopology: true}))
-        .then(() => {
-            console.log('Connected to MongoDB...')
-        })
+    useUnifiedTopology: true, 
+})
+.then(() => {
+        console.log('Connected to MongoDB...')
+    })
+.catch((err) => {
+    console.error(`Error connecting to MongoDB: \n${err}`);
+})
