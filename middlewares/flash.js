be718cb17cdbfc66f8a43f97e03ca65bab30fc5c
daudujohn <daudujoe@gmail.com>
2023-04-07 23:35:05 +0100

    chore: add flash middleware

---
+++ middlewares/flash.js
@@ -0,0 +1,10 @@
+const flash = (req, res, next) => {
+    if (req.session.flashData){
+        for (const key in req.session.flashData) {
+            res.locals[key] = req.session.flashData[key]
+        }
+    }
+    next();
+}
+
+module.exports = flash
