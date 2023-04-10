06012529c0a4739d5030c91640ea9975cf33eb51
daudujohn <daudujoe@gmail.com>
2023-04-10 23:12:31 +0100

    chore: add flash middleware
    The flash middleware retrieves flash data from the user's session and adds it to the response locals before deleting it from the session. This data can be used to display messages or to pre-fill form data. It prevents the user from submitting the same form twice when refreshing on a post method by redirecting them after the initial submission and clearing the flash data from the session.

--- routes/register.js
+++ routes/register.js
@@ -4,31 +4,38 @@ const UserController = require('../controllers/register');
 const {validateUser} = require('../models/user');
 const {joiFormatter, mongooseFormatter} = require('../utils/validationFormatter');
 const guest = require('../middlewares/guest');
+const flash = require('../middlewares/flash');
 
-router.get('/', guest, (req, res) => {
+
+router.get('/', [guest, flash], (req, res) => {
     res.render('register')
 })
 
-router.post('/', guest, async (req, res) => {
+router.post('/', [guest, flash], async (req, res) => {
     try{
         const {error} = validateUser(req.body)
-        if (error) return res.render('register', {
-            message: {
-                type: 'error', 
-                body: 'Validation Error'
-            },  
-            errors: joiFormatter(error), 
-            formData: req.body
-        })
+        if (error) {
+            req.session['flashData'] = {
+                    message: {
+                        type: 'error', 
+                        body: 'Validation Error'
+                    },  
+                    errors: joiFormatter(error), 
+                    formData: req.body
+                }
+
+            return res.redirect('/register')
+        }
 
         await UserController.addUser(req.body)
-        return res.render('register', {
+        req.session['flashData'] = {
             message: {
                 type: 'success', 
                 body: 'Registration Successful'
             },  
             formData: req.body
-        })
+        }
+        return res.redirect('/register')
     }
     catch(e){
         res.status(500).render('register', {
