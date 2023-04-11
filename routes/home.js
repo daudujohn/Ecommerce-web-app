5658d8afb6979a3e3591c70eaaa84936205073b3
daudujohn <daudujoe@gmail.com>
2023-04-11 13:20:33 +0100

    chore: name personalization for logged in user

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 
-router.get('/', auth, (req, res) => {
-    res.render('index')
+router.get('/', (req, res) => {
+  if (req.isAuthenticated()) return res.render('homepage', { name: req.user.name })
+  return res.render('index', {name: null})
 })
 
+router.post('/logout', auth, function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+});
 module.exports = router;
