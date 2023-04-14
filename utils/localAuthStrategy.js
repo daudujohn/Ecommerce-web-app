eb4ffd1820350225dfeee51744dbf45ebec74f3b
daudujohn <daudujoe@gmail.com>
2023-04-14 11:09:03 +0100

    feat(authentication): Add error message for invalid login
    displays specific error messages in non-production enviroment and vague error messages in production environment

--- utils/localAuthStrategy.js
+++ utils/localAuthStrategy.js
@@ -6,9 +6,9 @@ passport.use(new LocalStrategy({ 'usernameField': 'email' },
     async (email, password, done) => {
     try{
         const user = await User.findOne({ email })
-        if (!user) return done(null, false)
+        if (!user) return done(null, false, { loginError: process.env['NODE_ENV'] !== 'production' ? 'User not Found' : 'Invalid Username or Password' })
         if (await user.confirmPassword(password)) return done(null, user);
-        return done(null, false);
+        return done(null, false, { loginError: process.env['NODE_ENV'] !== 'production' ? 'Incorrect Password' : 'Invalid Username or Password' });
     }
     catch (e) {
         done(e)
