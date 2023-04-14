c4019ebfba51606c0b34da4c88710653397758c5
daudujohn <daudujoe@gmail.com>
2023-04-14 11:12:33 +0100

    feat: add flash messages to login route

--- routes/login.js
+++ routes/login.js
@@ -2,22 +2,47 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const guest = require('../middlewares/guest');
+const flash = require('../middlewares/flash');
 
-router.get('/', guest, (req, res) => {
+router.get('/', [guest, flash], (req, res) => {
     res.render('login')
 })
 
-router.post('/', guest, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-  }), 
-  (req, res) => {
-    res.render('login', {
-        message: {
-            type: 'success', 
-            body: 'Login Successful'
+router.post('/', [guest, flash], (req, res, next) => {
+ passport.authenticate('local', (err, user, info) => {
+    if (err) {
+        console.log('Err: ', err);
+        req.session['flashData'] = {
+            message: {
+                type: 'error', 
+                body: 'Something went wrong'
+            },
+            errors: {}
         }
+        return res.redirect('/login')
+    }
+    if (!user) {
+        req.session['flashData'] = {
+            message: {
+                type: 'error', 
+                body: info.loginError
+            },  
+        }
+        return res.redirect('/login')
+    }
+    req.logIn(user, (err) => {
+        if (err) {
+            req.session['flashData'] = {
+                message: {
+                    type: 'error', 
+                    body: 'Something went wrong'
+                },  
+            }
+            return res.redirect('/login')    
+        }
+        return res.redirect('/')
     })
+ })(req, res, next)
 })
 
 module.exports = router;
