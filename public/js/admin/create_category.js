19b5f1bc4e9787225dea27d283da016f0ccd4f67
daudujohn <daudujoe@gmail.com>
2023-05-24 16:22:04 +0100

    Revert "chore: add inputtypeoptions and addnewc"
    This reverts commit 2900140c9d0a7618a50e4d873fb1d7d3ad2d8355.

--- public/js/admin/create_category.js
+++ public/js/admin/create_category.js
@@ -1,63 +1,28 @@
 let app = new Vue({
-  el: "#app",
+    'el': '#app', 
 
-  data: {
-    category: {
-      name: "",
-      description: "",
-      seoDescription: "",
-      properties: [
-        {
-          name: "Storage",
-          required: true,
-          input: {
-            type: "",
-            propertyChoices: [{ label: "Android", value: "android" }],
-          },
-        },
-        {
-          name: "RAM",
-          required: true,
-          input: { type: "", propertyChoices: [{}] },
-        },
-      ],
-    },
-    inputTypeOptions: [
-      { label: "Fractional Input", value: "fractionalInput" },
-      { label: "Integer Input", value: "integerInput" },
-      { label: "Text", value: "textInput" },
-      { label: "Select (One)", value: "selectOne" },
-      { label: "Select (Multiple)", value: "selectMultiple" },
-    ],
-  },
+    data: {
+        category: {
+            name: '', 
+            description: '', 
+            seoDescription: '' ,
+            properties: [
+                {
+                    name: 'Storage'
+                }, 
+                {
+                    name: 'RAM'
+                }
+            ]
+        }
+    }, 
 
-  methods: {
-    saveCategory: function () {
-      console.log(this.category);
-    },
-    addProperty: function () {
-      this.category.properties.push(
-          {
-            name: "",
-            required: true,
-            input: {
-              type: "",
-              propertyChoices: [{ label: "", value: "" }],
-            },
-          },
-      )
-    },
-
-    removeProperty: function (index) {
-      this.category.properties.splice(index, 1);
-    },
-
-    addNewChoice: function(propertyChoices){
-        propertyChoices.push({
-            label: '', 
-            value: '', 
-
-        })
+    methods: {
+        saveCategory: function (){
+            console.log(this.category)
+        }, 
+        addProperty: function(){
+            this.category.properties.push({})
+        }
     }
-  },
-});
+})
