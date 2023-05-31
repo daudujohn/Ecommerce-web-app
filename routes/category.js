a728a1a690cb7603ebefd964eaac47c2c719417c
daudujohn <daudujoe@gmail.com>
2023-05-31 23:38:41 +0100

    chore: add post api route

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { addCategory } = require('../controllers/category');  
 
 router.get('/add', (req, res) => {
     res.render('category/add')
+})
+
+router.post('/api/v1/', async (req, res) => {
+    const category = await addCategory(req.body);
+    return res.json(category);
 })
 
 module.exports = router;
